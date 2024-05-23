178566f1494229ca8bc2adcd87dfde68db29dcaf
CarlosGbr11 <carlos157759@gmail.com>
2024-05-22 21:20:21 -0300

    Update Delete.jsx

--- frontend/src/components/Delete.jsx
+++ frontend/src/components/Delete.jsx
@@ -1,10 +1,31 @@
-import './Delete.css'
+import React, { useState, useEffect } from "react";
 
-const Delete = () =>{
-    return(
-        <div>
-            <p>Delete</p>
-        </div>
-    )
+function Delete() {
+
+    const [formValores, setFormValores] = useState({
+        id: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValores(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            console.log("Dados a serem enviados:", formValores.id);
+            const response = await fetch(`http://localhost:3000/pessoas/${formValores.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formValores)
+            });
+        }
+ }
 }
-export default Delete;
