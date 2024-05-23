a4e7b48e56e77eebc08c6a76431c4f36806083de
CarlosGbr11 <carlos157759@gmail.com>
2024-05-22 21:20:23 -0300

    Update Read.jsx

--- frontend/src/components/Read.jsx
+++ frontend/src/components/Read.jsx
@@ -1,11 +1,53 @@
-import './Read.css'
+import React, { useState, useEffect } from "react";
 
-const Update = () =>{
+function Read() {
 
-  return(
+  const [consultaDados, setconsultaDados] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3000/pessoas', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const data = await response.json();
+      setconsultaDados(data);
+    } catch (err) {
+      console.error("Erro ao buscar dados no banco", err);
+    }
+  };
+
+  useEffect(() => {
+    handleSubmit(); 
+  }, []);
+
+  return (
     <div>
-      <p>Read</p>
+      <form onSubmit={handleSubmit}>
+        <button type="submit">Ler dados do banco</button>
+      </form>
+
+      <ol>
+        {consultaDados.map((linha, index) => (
+          <li key={index}>
+            <span>Nome: {linha.nome}</span>
+            <br />
+            <span>Data Nascimento: {linha.dataNascimento} </span>
+            <br />
+            <span>Genero: {linha.genero} </span>
+            <br />
+            <span>CPF: {linha.cpf} </span>
+            <br />
+            <span>Telefone: {linha.telefone} </span>
+          </li>
+        ))}
+      </ol>
     </div>
   )
 }
-export default Update;
+export default Read;
