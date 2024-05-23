91ff5152a99b6ff6a98255896b5db3eb9723f0f4
CarlosGbr11 <carlos157759@gmail.com>
2024-05-23 00:15:29 -0300

    Update Delete.jsx

--- frontend/src/components/Delete.jsx
+++ frontend/src/components/Delete.jsx
@@ -26,6 +26,22 @@ function Delete() {
                 },
                 body: JSON.stringify(formValores)
             });
+
+            const json = await response.json();
+            console.log(response)
+            console.log(json);
+        } catch (err) {
+            console.error("Erro ao enviar", err)
         }
- }
+ };
+
+ return(
+    <form onSubmit={handleSubmit}>
+        ID a ser deletado:
+        <input type="text" name="id" value={formValores.id} onChange={handleChange}/>
+        <br />
+        <button type="submit" onClick={handleSubmit}>Deletar</button>
+    </form>
+ );
 }
+export default Delete;
