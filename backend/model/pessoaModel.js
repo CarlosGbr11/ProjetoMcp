cd3cce6f14a7ca0d29d0a38a375dcdbcc575610d
CarlosGbr11 <carlos157759@gmail.com>
2024-05-24 21:17:10 -0300

    Update pessoaModel.js

--- backend/model/pessoaModel.js
+++ backend/model/pessoaModel.js
@@ -4,11 +4,11 @@ export function read(callback){
     connection.query('SELECT * from pessoas_dados', callback);
 }
 export function create(nome,dataNascimento,genero,cpf,telefone,callback){
-    connection.query('INSERT INTO pessoas_dados (nome, dataNascimento, genero, cpf, telefone) VALUES (?, ?, ?, ?, ?)', [nome,dataNascimento,genero,cpf,telefone], callback);
+    connection.query('INSERT INTO pessoas_dados (nome, dataNascimento, genero, cpf, telefone, ativo_pessoa) VALUES (?, ?, ?, ?, ?,1)', [nome,dataNascimento,genero,cpf,telefone], callback);
 }
-export function update(id, novoDados, callback) {
-    connection.query('UPDATE pessoas_dados set ? where id = ?', [novoDados,id], callback);
+export function update(nome, dataNascimento, genero, cpf, telefone, id, callback) {
+    connection.query("UPDATE pessoas_dados SET nome = ?, dataNascimento = ?, genero = ?, cpf = ?, telefone = ?, ativo_pessoa = 1 WHERE idpessoa = ?", [nome, dataNascimento, genero, cpf, telefone, id], callback);
 }
 export function deletePes(id, callback) {
-    connection.query('DELETE from pessoas_dados where id_pessoa = ?', [id], callback);
+    connection.query('DELETE from pessoas_dados where idpessoa = ?', [id], callback);
 }
